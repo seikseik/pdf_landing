c60c22bfdc23eafc0d66ca540a310e556f83d842
Matteo Sacchi <matteoseik@hotmail.it>
2022-05-09 14:16:24 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import background from '../images/content/background.mp4';
 import foreground from '../images/content/foreground.mp4';
+import back_mobile from '../images/content/back_mobile.mp4';
 
 
 import '../scss/app.scss';
